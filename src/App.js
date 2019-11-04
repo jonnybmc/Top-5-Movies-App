d7c4d833381684c7d9b04e96a008fa9da89ce420
jonathanbooysen <jonnybmc@gmail.com>
2019-11-04 05:08:21 +0200

    setup react router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from './routes/home';
+import MovieDetail from './routes/movieDetail';
+
+import {BrowserRouter as Router,Route,Link} from 'react-router-dom';
+
+class App extends Component {
+  render(){
+    return (
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/"> Home </Link>
+            </li>
+            <li>
+              <Link to="/:id"> MovieDetail </Link>
+            </li>
+          </ul>
+        </div>
+
+        <Route exact path="/" component= {Home}></Route>
+        <Route exact path="/:id" component= {MovieDetail}></Route>
+      </Router>
+    )
+  }
 }
+
 
 export default App;
---
+++ src/routes/movieDetail/movieDetail.js
@@ -0,0 +1,7 @@
+import React, {Component} from 'react';
+
+function MovieDetail(){
+    return <h1>This is the MovieDetail</h1>
+}
+
+export default MovieDetail;
